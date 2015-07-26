7e836f550ac92823fcb4da071b8f7339dac100e7
Benj Arriola <benj@benjarriola.com>
2015-07-26 01:42:41 -0700

    This is a test if my local Github is syncing.
    Commiting to the changes, it was just a comment for testing.

--- cachematrix.R
+++ cachematrix.R
@@ -1,4 +1,5 @@
-## OMG, still getting used to git... and deadlines coming up...
+## Edit in browser: OMG, still getting used to git... and deadlines coming up...
+## Edit in my local RStudio, with my github synced up.
 ## Put comments here that give an overall description of what your
 ## functions do
 
